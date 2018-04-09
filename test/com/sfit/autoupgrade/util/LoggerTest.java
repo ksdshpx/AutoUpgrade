326b29756fd4fa0453cf70af11204f3d1fdb42d0
ksdshpx <southeast_px@163.com>
2018-04-09 23:10:55 +0800

    项目加入Junit单元测试框架！

---
+++ test/com/sfit/autoupgrade/util/LoggerTest.java
@@ -0,0 +1,17 @@
+package com.sfit.autoupgrade.util;
+
+import org.junit.Test;
+
+/**
+ * Create with IntelliJ IDEA
+ * Create by peng.x
+ * Date: 2018/4/9
+ * Time: 23:08
+ * Description:使用Junit进行单元测试
+ */
+public class LoggerTest {
+    @Test
+    public void testLog(){
+        Logger.log("使用Junit测试Logger日志类");
+    }
+}
