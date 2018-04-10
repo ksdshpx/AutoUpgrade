ef81d91e9c910228e2c10bec77ea3c84d437bd31
ksdshpx <southeast_px@163.com>
2018-04-10 09:34:12 +0800

    创建服务器端套接字，并绑定端口号

--- src/com/sfit/autoupgrade/core/BootStrap.java
+++ src/com/sfit/autoupgrade/core/BootStrap.java
@@ -1,5 +1,10 @@
 package com.sfit.autoupgrade.core;
 
+import com.sfit.autoupgrade.util.Logger;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
 /**
  * Create with IntelliJ IDEA
  * Create by peng.x
@@ -20,6 +25,12 @@ public class BootStrap {
      * 主程序入口
      */
     private static void start() {
-
+        //创建服务器端套接字，并绑定端口
+        try {
+            Logger.log("AutoUpgrade Start!");
+            ServerSocket serverSocket = new ServerSocket(8888);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
